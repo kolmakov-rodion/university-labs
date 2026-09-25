9f5647831af2c249a6d4ec7a8858efd5063b22f7
Колмаков Родион <rodionkolmakov36@gmail.com>
2026-09-25 14:43:59 +0300

    Task 14

--- ЛР2/python_lab_02/main.py
+++ ЛР2/python_lab_02/main.py
@@ -1,8 +1,5 @@
-age = 18
-has_access = False
-print(f"Возраст: {age}")
-print(f"Допуск имеется: {has_access}")
-if age >= 18 and has_access:
-    print("Доступ разрешён")
-else:
-    print("Доступ запрещён")
+n = int(input())
+sm = 0
+for _ in range(n):
+    sm += float(input())
+print(sm, sm / n)
