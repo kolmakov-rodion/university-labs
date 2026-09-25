9d16edd054677c7f1a5ce4ffdfd32ae200c97374
Колмаков Родион <rodionkolmakov36@gmail.com>
2026-09-25 12:58:19 +0300

    Task 4

--- ЛР2/python_lab_02/main.py
+++ ЛР2/python_lab_02/main.py
@@ -1,1 +1,13 @@
-print("Hello, World!")
+name = 'Vasya'
+age = 19
+height = 180.1
+is_student = False
+
+print(name)
+print(age)
+print(height)
+print(is_student)
+print(type(name))
+print(type(age))
+print(type(height))
+print(type(is_student))
